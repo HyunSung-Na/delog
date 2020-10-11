a6b45902f20e54e2785437b5788f9eafea3a4a1d
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2020-10-11 18:05:15 +0900

    first commit

---
+++ src/test/java/com/devlog/delog/DelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlog.delog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
